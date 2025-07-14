7d26525b1e941db817b6fdbbea2116e7e5857448
David <rynstheoverlord@gmail.com>
2025-07-14 23:10:07 +0100

    enabled creation of store

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -19,10 +20,39 @@ func main() {
 		ctx.HTML(http.StatusOK, "index.html", nil)
 	})
 
+	router.POST("/", homePostRouteFunc)
+
 	router.GET("/login", func(ctx *gin.Context) {
 		var _ = sessions.Default(ctx)
 		ctx.HTML(http.StatusOK, "login.html", nil)
 	})
 
+	router.GET("/store", func(ctx *gin.Context) {
+		session := sessions.Default(ctx)
+		storeName := session.Get("storename")
+		storeId := session.Get("storeid")
+
+		if storeName == nil || storeId == nil {
+			ctx.Redirect(http.StatusMovedPermanently, "/login")
+		}
+
+		ctx.HTML(http.StatusOK, "store.html", gin.H{
+			"storeName": storeName,
+			"storeId":   storeId,
+		})
+	})
+
 	router.Run()
+}
+
+func homePostRouteFunc(ctx *gin.Context) {
+	session := sessions.Default(ctx)
+	storeName := ctx.PostForm("storename")
+	storeId := uuid.New().String()
+
+	session.Set("storename", storeName)
+	session.Set("storeid", storeId)
+
+	session.Save()
+	ctx.Redirect(http.StatusMovedPermanently, "/store")
 }
